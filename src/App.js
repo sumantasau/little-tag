30d541468afc264cf8faab8168df75adde6f7c9d
sumantasau <sumantasau@gmail.com>
2021-05-04 20:53:55 +0530

    stripe payment gateway implementation to get token

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import "./styles.css";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe("pk_test_51InNdOSEbCsj1Q2Hs8etAGQSQtsRJoXUhTSTiNgO3I8qlh9vrCzQ2RhDuB7SS0oYRN7TH9Fu0EqjiLADNa9GKQle00RQw6LRyg");
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +24,21 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header>*/}
+  
+      <div className="product">
+        <img
+          src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress"
+          alt="laptop"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <div>
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </div>
+    
     </div>
   );
 }
